2874d97f1c55febba1d3a6739fc278d0a448eb21
Carolina Caires <carolbazarello@gmail.com>
2017-02-25 17:46:37 -0300

    Commit inicial

---
+++ js/default.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    
+    var par = getQueryParams(location.search);
+    var receita = par.receita;
+
+    $('.receita').hide();
+    $('#rec_'+receita).show();
+});
+
+function getQueryParams(qs) {
+    qs = qs.split('+').join(' ');
+
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+}
